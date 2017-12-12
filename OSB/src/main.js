67fa97d68010eed3ff4f9343fcbdf605e7a660b7
bozhi.lai <bozhi.lai@vipshop.com>
2017-12-12 16:43:50 +0800

    [A] use font and osb

--- OSB/src/main.js
+++ OSB/src/main.js
@@ -3,8 +3,12 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import OSB from './osb'; 
+import FONT from './font'; 
 
 Vue.config.productionTip = false;
+
+Vue.use(OSB); 
 
 /* eslint-disable no-new */
 new Vue({
