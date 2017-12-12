0beb0b8a93d9dec0326d1a2a2cff7b2f3c41b227
eczn <1975845422@qq.com>
2017-12-12 23:16:59 +0800

    [A] api/* ( /api/nfs/* )

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,13 +1,19 @@
 const express = require('express')
     , router = express.Router()
     , nfs = require('./nfs')
-
+    , user = require('./user')
+    , all = require('./all')
 
 router.get('/ping', function(req, res){
     res.json('pong')
 });
 
+router.use('/user', user); 
+
+router.use('*', all); 
+
 router.use('/nfs', nfs); 
+
 
 
 module.exports = router;
--- server/routes/api/nfs.js
+++ server/routes/api/nfs.js
@@ -2,9 +2,96 @@ const express = require('express')
     , router = express.Router()
     , rps = require('../../utils/rps')
     , diskSet = require('../../utils/disk-set')
+    , temp = require('../../utils/temp_file')
 
+/**
+ * @description get user's disk 
+ */
 router.get('/', function(req, res, next) {
-    rps.send2000(res, diskSet.list()); 
+    let user_id = req.user._id; 
+    
+    diskSet.list().then(myDisks => {
+        rps.send2000(res, myDisks); 
+    }).catch(err => {
+        console.log(err); 
+        rps.send5000(res, err); 
+    });
+});
+
+/**
+ * @description create a disk for user 
+ */
+router.post('/', function(req, res){
+    let user_id = req.user._id; 
+
+    diskSet.create(user_id).then(disk_config => {
+        rps.send2000(res, disk_config); 
+    }).catch(err => {
+        console.log(err); 
+
+        rps.send5000(res, err); 
+    }); 
+}); 
+
+/**
+ * @description find nfs and pass cmd to process 
+ */
+router.post('/cmd', function(req, res){
+    let _id = req.body._id;
+    let cmd = req.body.cmd.trim(); 
+    let buf_base64 = req.body.buf_base64; 
+    let preprocess  = req.body.preprocess; 
+    let list = cmd.split(' '); 
+
+    // 如果有 buf 数据 
+    if (buf_base64){
+        let buf = Buffer.from(buf_base64, 'base64'); 
+        let position = list.indexOf('%buf%'); 
+        list[position] = buf; 
+    }
+
+    let todo = list[0]; 
+    let args = list.slice(1); 
+
+    diskSet.open(_id).then(nfs => {
+        return nfs[todo].apply(nfs, args); 
+    }).then(nfs_result => {
+        // console.log(nfs_result); 
+        if (Buffer.isBuffer(nfs_result)){
+            if (preprocess === 'url'){
+                temp.add(nfs_result).then(url => {
+                    res.json({
+                        code: 2000, 
+                        data: nfs_result.toJSON(), 
+                        url: url, 
+                        msg: 'nfs 调用成功'
+                    }); 
+                }); 
+            } else if (preprocess === 'string'){
+                res.json({
+                    code: 2000, 
+                    data: nfs_result.toString(), 
+                    msg: 'nfs 调用成功'
+                }); 
+            } else {
+                res.json({
+                    code: 2000, 
+                    data: nfs_result.toJSON(), 
+                    msg: 'nfs 调用成功'
+                }); 
+            }
+        } else {
+            res.json({
+                code: 2000, 
+                data: nfs_result || null, 
+                msg: 'nfs 调用成功'
+            }); 
+        }
+    }).catch(err => {
+        console.log(err); 
+
+        rps.send5000(res, err)
+    })
 });
 
 module.exports = router;
