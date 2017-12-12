08fb378b8e44c558ce87029d1d9139bcfab43262
eczn <1975845422@qq.com>
2017-12-12 23:16:19 +0800

    [M] finish disk-set

--- server/utils/disk-set.js
+++ server/utils/disk-set.js
@@ -1,14 +1,48 @@
 // disk-set.js
+const { diskModel } = require('./db')
+    , set = {}
+    , N = require('../../core')
+    , diskTable = {}
 
-let set = {}; 
-let disk_list = []; 
+set.create = function(user_id){
+    let obj = {}; 
+    obj.owner = user_id; 
 
-set.init = function(){
+    obj = new diskModel(obj); 
 
+    return obj.save().then(disk_config => {
+        disk_config = disk_config.toObject(); 
+    
+        return N(disk_config).then(nfs => {
+            diskTable[disk_config._id] = nfs; 
+
+            return disk_config; 
+        }); 
+    }); 
 }
 
-set.list = function(){
-    return disk_list; 
+set.list = function(user_id){
+    return diskModel.find({
+        user_id
+    }); 
+}
+
+set.open = function(_id){
+    if (diskTable[_id]){
+        return Promise.resolve(diskTable[_id]); 
+    } else {
+        return diskModel.findOne({ _id }).then(disk_config => {
+            if (!disk_config) return Promise.reject('NOT FOUND'); 
+
+            disk_config = disk_config.toObject(); 
+        
+            return N(disk_config).then(nfs => {
+                diskTable[disk_config._id] = nfs; 
+
+                return nfs; 
+            }); 
+        })
+    }
 }
 
 module.exports = set; 
