17cb395eaf5e13be79812659fcebd3a878e95ee4
bozhi.lai <bozhi.lai@vipshop.com>
2017-12-12 13:56:46 +0800

    [M] nfs_minimun

--- core/Disk/nfs_minimum.js
+++ core/Disk/nfs_minimum.js
@@ -5,21 +5,9 @@ module.exports = {
     // F-Tree 
     root: {
         isDirectory: true,
-        files: [
-            {
-                filename: 'hello', 
-                ext: 'txt',
-                isDirectory: false, 
-                cTime: Date.now(), 
-                size: 10,
-                A1: [ 0 ]
-            }
-        ]
+        cTime: Date.now(), 
+        files: []
     }, 
     // File Allocation 
-    FAT: (function(){
-        let arr = new Array(BLOCK_COUNT).fill(0); 
-        arr[0] = 1; 
-        return arr; 
-    })()
+    FAT: new Array(BLOCK_COUNT).fill(0)
 }
