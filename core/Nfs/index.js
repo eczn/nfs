21871f1466f36652686308d28001c5e18b17dadc
bozhi.lai <bozhi.lai@vipshop.com>
2017-12-12 11:08:39 +0800

    [A] FAT & read & write

--- core/Nfs/index.js
+++ core/Nfs/index.js
@@ -1,5 +1,8 @@
+import { Buffer } from 'buffer';
+
 // nfs.js
-const error = require('./error'); 
+const error = require('./error')
+    , fatController = require('./fat_controller')
 
 /**
  * @description 构造实例  
@@ -9,6 +12,55 @@ const error = require('./error');
 function Nfs(nfs_data, disk){
     this.disk = disk; 
     Object.assign(this, nfs_data); 
+}
+
+/**
+ * @description 申请一定空间的块  
+ * @param {int} size 
+ */
+Nfs.prototype.fatAlloc = function(size){
+    let BLOCK_SIZE = this.disk.BLOCK_SIZE; 
+    let need_block = Math.ceil(BLOCK_SIZE / size); 
+    
+    return this.fatAllocBlock(need_block); 
+}
+
+/**
+ * @description 申请一定数量的块
+ * @param {int} need_block 
+ */
+Nfs.prototype.fatAllocBlock = function(need_block){
+    let FAT = this.FAT; 
+    let res = []; 
+
+    for (let i = 0; i < FAT.length; i++){
+        let info = FAT[i]; 
+        if (info === 0){
+            res.psuh(i); 
+        }
+        // 如果收集够了就结束 
+        if (res.length === need_block){
+            break; 
+        }
+    }
+
+    // 设置 FAT 对应位置为 已占用 
+    res.forEach(i => {
+        // 已占用 
+        FAT[i] = 1; 
+    }); 
+    return res; 
+}
+
+/**
+ * @description 释放 fat 
+ * @param {Array<int>} A1 
+ */
+Nfs.prototype.fatRelease = function(A1){
+    let FAT = this.FAT; 
+    A1.forEach(a => {
+        FAT[a] = 0; 
+    }); 
 }
 
 /**
@@ -93,22 +145,18 @@ Nfs.prototype.store2disk = function(){
     ); 
 }
 
-Nfs.prototype.getA2List = function(A2){
-    
-}
-
+/**
+ * @description 获取文件所在的区块 
+ * @param {Object} node 
+ */
 Nfs.prototype.getFileIndex = function(node){
-    let BLOCK_SIZE = this.disk.BLOCK_SIZE; 
-    
-    if (node.size <= BLOCK_SIZE){
-        return [node.A1]; 
-    } else {
-        return [node.A1].concat(
-            this.getA2List(node.A2)
-        ); 
-    }
+    return node.A1; 
 }
 
+/**
+ * @description 读取某个文件、或者目录 
+ * @param {String} path_str 
+ */
 Nfs.prototype.read = function(path_str){
     let file_node = this.ls(path_str); 
 
@@ -121,6 +169,57 @@ Nfs.prototype.read = function(path_str){
             file_node.size
         ); 
     }
+}
+
+/**
+ * @description 覆盖写入数据到某文件、如果是目录则会报错 4002  
+ * @param {*} path_str 
+ * @param {*} buf_data 
+ */
+Nfs.prototype.write = function(path_str, buf_data){
+    let file_node = this.ls(path_str); 
+
+    // 不是 Buffer 返回 0 
+    if (!Buffer.isBuffer(buf_data)) return Promise.resolve(0);
+    // 错误 
+    if (file_node.isDirectory) return error(4002, [path_str]); 
+        
+    let BLOCK_SIZE = this.disk.BLOCK_SIZE; 
+    let newSize = buf_data.length; 
+    let newBlockCount = Math.ceil(newSize / BLOCK_SIZE); 
+
+    let preSize = file_node.size;
+    let preBlockCount = Math.ceil(preSize / BLOCK_SIZE); 
+
+    let preA1 = file_node.A1; 
+    let deltaCount = newBlockCount - preBlockCount; 
+
+    if (deltaCount >= 0){
+        // 连接 申请 增长 
+        let inc = this.fatAllocBlock(deltaCount); 
+        file_node.A1 = preA1.concat(inc); 
+    } else {
+        // 截短 释放 缩短 
+        file_node.A1 = preA1.slice(0, cha); 
+        let toRelease = preA1.slice(cha); 
+
+        this.fatRelease(toRelease); 
+    }
+
+    file_node.size = newSize; 
+
+    /**
+     * Write Data 
+     */
+    return this.store2disk().then(ok => {
+        return this.disk.writeList(
+            file_node.A1, 
+            buf_data
+        );
+    }).catch(err => {
+        console.log(err); 
+    }); 
+    
 }
 
 /**
@@ -147,8 +246,7 @@ Nfs.prototype.touch = function(path_str, ext){
             isDirectory: false,
             cTime: Date.now(), 
             size: 0, 
-            A1: null, 
-            A2: null
+            A1: []
         }); 
     }
 
