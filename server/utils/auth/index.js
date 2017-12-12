28dd3b1f323060fe931d215362ddd85ba603a9c3
eczn <1975845422@qq.com>
2017-12-12 23:15:29 +0800

    [A] auth for user login & cookie

--- server/utils/auth/index.js
+++ server/utils/auth/index.js
@@ -1,7 +1,6 @@
 // index.js
 const jwt = require('jsonwebtoken')
-    , SECRET = `This_Is_Secret_Key`
-    , R = require('../redis')
+    , SECRET = `nfs-secret-key`
 
 let auth = {}; 
 
@@ -12,36 +11,20 @@ auth.en = function(data = {}){
 		data: data
 	}, SECRET);
 
-	// 缓存 
-	// console.log(data); 
-	R.set(token, JSON.stringify(data));
-
 	return token; 
 }
 
 auth.de = function(token){
 	let data;
 
-	return R.get(token).then(val => {
-		if (val){
-			// 缓存命中 
-			return JSON.parse(val); 
-		} else {
-			// 否则尝试解开 
-			try {
-				opt = jwt.verify(token, SECRET);
+	// 否则尝试解开 
+	try {
+		opt = jwt.verify(token, SECRET);
 
-				// 重置缓存 
-				R.set(token, JSON.stringify(opt.data));
-
-				return opt.data; 
-			} catch (err) {
-				// 如果出错 证明该 Token 不合法 
-				console.log(err)
-				return Promise.reject(false); 
-			}
-		}
-	})
+		return Promise.resolve(opt.data); 
+	} catch (err) {
+		return Promise.reject(false); 
+	}
 }
 
 module.exports = auth; 
