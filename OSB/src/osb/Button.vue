7f926a867125fd487f587de141073f00c1ff7538
bozhi.lai <bozhi.lai@vipshop.com>
2017-12-13 14:58:06 +0800

    [A] base btn

---
+++ OSB/src/osb/Button.vue
@@ -0,0 +1,45 @@
+<template>
+    <div :style="css" @click="clicking" class="button-wrap">{{ text }}</div>
+</template>
+
+<script>
+export default {
+    name: 'osb-btn',
+    props: {
+        text: {
+            type: String, 
+            default: '标题'
+        },
+        css: {
+            type: Object, 
+            default(){
+                return {
+                    backgroundColor: '#FFF', 
+                    color: '#555', 
+                    border: '1px solid #DDD', 
+                    padding: '.3em',
+                    minWidth: '40%',
+                    borderRadius: '4px'
+                }
+            }
+        }
+    }, 
+    data(){
+        return {}
+    },
+    created(){
+
+    },
+    methods: {
+        clicking(e){
+            this.$emit('click', e); 
+        }
+    }
+}
+</script>
+
+<style lang="sass">
+.button-wrap 
+    display: inline-block
+</style>
+
--- OSB/src/osb/index.js
+++ OSB/src/osb/index.js
@@ -1,11 +1,13 @@
 // index.js
 import Input from './Input'; 
+import Button from './Button'; 
 
 let osb = {}; 
 
 
 osb.install = function(Vue, config){
     Vue.component('osb-input', Input); 
+    Vue.component('osb-btn', Button); 
 }
 
 export default osb; 
