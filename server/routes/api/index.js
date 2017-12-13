2516077b9344d81dfbcfe3b2206909a0d8fa809d
bozhi.lai <bozhi.lai@vipshop.com>
2017-12-13 10:34:32 +0800

    [M] 调整结构

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -4,6 +4,21 @@ const express = require('express')
     , user = require('./user')
     , all = require('./all')
 
+router.use('*', function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+	res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+	res.header("X-Powered-By",'ECZN@NIGHTWATCH'); 
+	res.header("Content-Type", "application/json;charset=utf-8");
+	res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+
+    if (req.method === "OPTIONS") {
+		res.sendStatus(200);
+	} else {
+        next(); 
+    }
+})
+
 router.get('/ping', function(req, res){
     res.json('pong')
 });
