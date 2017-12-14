fd52d608bea9267a82a2926fe8654dad0967d120
eczn <1975845422@qq.com>
2017-12-14 13:23:56 +0800

    [A] 图标

---
+++ OSB/src/assets/icon.js
@@ -0,0 +1,26 @@
+// index.js
+import disk from './disk.png'; 
+import diskCreate from './disk-create.png'; 
+// import fileIcon from './file-icon.png'; 
+import folder from './folder.png'; 
+// import folderIcon from './folder-icon.png'; 
+// import chrome from './google-chrome.png'; 
+import jpg from './jpg.png'; 
+import png from './jpg.png'; 
+import mail from './mail.png'; 
+import txt from './txt.png';
+// import zip from './zip.png'; 
+
+export default {
+    disk,
+    diskCreate, 
+    // fileIcon,
+    folder,
+    // folderIcon,
+    // chrome,
+    jpg,
+    png,
+    mail,
+    txt,
+    // zip
+}
